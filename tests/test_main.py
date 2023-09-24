b7c424660c859699cb860433d3791033000c0cc0
vedranson <vedranson@gmail.com>
2023-09-24 21:10:25 +0200

    update test_main.py

--- tests/test_main.py
+++ tests/test_main.py
@@ -1,2 +1,10 @@
-def test_main():
-    assert True
+from fastapi.testclient import TestClient
+from app.main import app
+
+client = TestClient(app)
+
+
+def test_root():
+    response = client.get('/')
+    assert response.status_code == 200
+    assert response.json() == {'message': 'root'}
