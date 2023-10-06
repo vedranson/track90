ba15106511e012e05afad6d847bd91df74ebfcd2
vedranson <vedranson@gmail.com>
2023-10-06 12:49:17 +0200

    add database tests

---
+++ tests/test_database.py
@@ -0,0 +1,74 @@
+import os
+import pytest
+from psycopg2 import connect, Error, OperationalError
+
+TEST_DATABASE_NAME = 'test_database'
+
+
+@pytest.mark.dependency()
+def test_postgres_password_in_environment():
+    if 'POSTGRES_PASSWORD' not in os.environ:
+        pytest.fail('POSTGRES_PASSWORD not found in environment.')
+
+
+@pytest.mark.dependency(depends=['test_postgres_password_in_environment'])
+def test_postgres_server_access_fails_when_nonexistent_user():
+    with pytest.raises(OperationalError) as exc_info:
+        connect(
+            host='localhost',
+            port=5432,
+            user='the_most_likely_nonexistent_user',
+            password=os.environ.get('POSTGRES_PASSWORD'),
+        )
+    assert str(exc_info.value) == (
+        'connection to server at "localhost" (::1), port 5432 failed: '
+        'FATAL:  password authentication failed for user '
+        '"the_most_likely_nonexistent_user"\n'
+    )
+
+
+@pytest.mark.dependency(depends=['test_postgres_password_in_environment'])
+def test_postgres_server_access():
+    try:
+        connect(
+            host='localhost',
+            port=5432,
+            user='postgres',
+            password=os.environ.get('POSTGRES_PASSWORD'),
+        )
+    except Error as e:
+        pytest.fail(f'Server access failed {e}')
+
+
+@pytest.mark.dependency(depends=['test_postgres_server_access'])
+def test_create_drop_databank():
+    stmt = f"DROP DATABASE IF EXISTS \"%(db_name)s\";"
+    try:
+        execute_sql(stmt)
+    except Error as e:
+        pytest.fail(f"Error drop_database_if_exists: {e}")
+
+    stmt = f"CREATE DATABASE \"%(db_name)s\";"
+    try:
+        execute_sql(stmt)
+    except Error as e:
+        pytest.fail(f"Error create_database: {e}")
+
+    stmt = f"DROP DATABASE \"%(db_name)s\";"
+    try:
+        execute_sql(stmt)
+    except Error as e:
+        pytest.fail(f"Error drop_database: {e}")
+
+
+def execute_sql(stmt):
+    conn = connect(
+        host='localhost',
+        port=5432,
+        user='postgres',
+        password=os.environ.get('POSTGRES_PASSWORD'),
+    )
+    conn.autocommit = True
+    with conn.cursor() as cursor:
+        stmt = f"DROP DATABASE IF EXISTS \"%(db_name)s\";"
+        cursor.execute(stmt, {'db_name': TEST_DATABASE_NAME})
